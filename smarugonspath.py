2eff22f8632be583c4942ca5fd4200df9833f466
fking <97300862+fkingerd@users.noreply.github.com>
2023-11-28 15:46:48 +0200

    Update and rename smarugonspath to smarugonspath.py

--- smarugonspath.py
+++ smarugonspath.py
@@ -16,44 +16,6 @@ def fetch_data(address):
 # Inside the 'if st.button('Fetch Data'):' block
 address = st.text_input('Enter the Address:', '')
 
-# Inside the 'if st.button('Fetch Data'):' block
-# if st.button('Fetch Data'):
-#     if len(address) == 62:
-#         data = fetch_data(address)
-#         if data:
-#             body_groups = {}
-#             for item in data:
-#                 body_value = next((attr['value'] for attr in item['metadata']['attributes'] if attr['trait_type'] == 'BODY'), None)
-#                 if body_value:
-#                     if body_value not in body_groups:
-#                         body_groups[body_value] = {'count': 0, 'items': []}
-#                     body_groups[body_value]['count'] += 1
-#                     body_groups[body_value]['items'].append(item)
-
-#             # Create full-width columns for dropdowns
-#             cols = st.columns(len(body_groups))
-#             for i, (body_type, info) in enumerate(body_groups.items()):
-#                 with cols[i].container():
-#                     expander = st.expander(f"{body_type} (Count: {info['count']})", expanded=False)
-#                     with expander:
-#                         for item in info['items']:
-#                             # Displaying only small image and item number
-#                             img_col, data_col = st.columns([1, 2])
-#                             with img_col:
-#                                 response = requests.get(item['media'][0]['thumbnailUrl'])
-#                                 if response.status_code == 200:
-#                                     img = Image.open(BytesIO(response.content))
-#                                     img.thumbnail((img.width // 5, img.height // 5))
-#                                     st.image(img, use_column_width=True)
-#                             with data_col:
-#                                 # Extract and display the item number from the 'name' key
-#                                 item_number = item['name'].split("#")[-1] if "#" in item['name'] else item['name']
-#                                 st.write(f"BEEF: #{item_number}")
-#         else:
-#             st.error("Data not found or API error!")
-#     else:
-#         st.error("Invalid address length!")
-# Inside the 'if st.button('Fetch Data'):' block
 if st.button('Fetch Data'):
     if len(address) == 62:
         data = fetch_data(address)
